9eedcef421bfee68dc62bba35d07d48714b56a2d
NRotondaro <niksonrotondaro1@gmail.com>
2023-06-09 12:37:52 -0300

    feat: add search content page

---
+++ components/SearchInput.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import queryString from 'query-string';
+
+import useDebounce from '@/hooks/useDebounce';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import Input from './Input';
+
+export const SearchInput = () => {
+  const router = useRouter();
+  const [value, setValue] = useState<string>('second');
+  const debouncedValue = useDebounce(value, 500);
+
+  useEffect(() => {
+    const query = { title: debouncedValue };
+
+    const url = queryString.stringifyUrl({ url: '/search', query: query });
+
+    router.push(url);
+  }, [debouncedValue, router]);
+  return (
+    <Input
+      placeholder='What do you want to listen to ?'
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
+};
